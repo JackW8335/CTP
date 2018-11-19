5bbd0aa555f4652905ed0c8953b47c4f97fa300c
Jack Watson <uohane@gmail.com>
2018-11-19 12:37:50 +0000

    Copied over personal project to github

---
+++ Assets/Scripts/CutInHalf.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutInHalf : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        
+        GameObject[] pieces = MeshCut.Cut(gameObject, transform.position, transform.right, GetComponent<Renderer>().material);
+
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            if (!pieces[i].GetComponent<BoxCollider>())
+                pieces[i].AddComponent<BoxCollider>();
+
+            if (!pieces[i].GetComponent<Rigidbody>())
+                pieces[i].AddComponent<Rigidbody>();
+        }
+    }
+}
---
+++ Assets/Scripts/Destrucible.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destrucible : MonoBehaviour
+{
+    public GameObject DestroyedVariant;
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 2)
+        {
+            Instantiate(DestroyedVariant, transform.position, transform.rotation);
+
+            Destroy(gameObject);
+        }
+    }
+}
